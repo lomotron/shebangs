4ca376b2bd9e0c179e4772d0bab54cbe2167452c
lomotron <silvio.wolf@gmail.com>
2024-11-18 00:24:58 +0100

    version with ars and stdin

--- runpod.sh
+++ runpod.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/awk BEGIN{exit system("bash -euo pipefail -c 'f=\"" ARGV[1] "\";n=$(awk \"/^#!\\/bin\\/bash$/ {print NR}\" \"$f\" ); t=$(sed -e \"s/^# //\" -ne \"2,$((n-1))p\" \"$f\"); s=$(tail +$n \"$f\") ;  eval \"echo \\\"$t\\\" \" | bash -eu'")}
-# timout=300
-# name="adhoc-\$HOSTNAME-\$USER-$(basename $f| sed "s/[^[:alnum:]]/-/g")-\$timout"
+#!/usr/bin/env -S bash -euo pipefail -c 'f="$0";n=$(awk "/^#!.+$/ {n=NR}END{print n}" "$f" ); t=$(sed -e "s/^# //" -ne "2,$((n-1))p" "$f"); s=$(tail +$n "$f" | base64) ; i="$(read -t 0 && base64)" eval "echo \\"$t\\"" | bash -eu'
+# timeout=300
+# mount=/mnt
+# name="adhoc-\$HOSTNAME-\$USER-$(basename $f| sed "s/[^[:alnum:]]/-/g")-\$timeout"
 # kubectl get job \$name &> /dev/null && kubectl delete job \$name > /dev/null
 # kubectl apply -f - <<EOF > /dev/null
 # apiVersion: batch/v1
@@ -12,33 +13,40 @@
 #     spec:
 #       containers:
 #       - name: \${name}-cont
-#         image: centos
+#         image: alpine
 #         command:
 #         - sh
 #         - "-c"
-#         - |
-#           /bin/sh <<LOL
-# $(echo "$s"| sed 's/^/          /')
-#           LOL
+#         - "test -f \$mount/stdin \&\& cat \$mount/stdin \| \$mount/script $@ \|\| \$mount/script $@"
+#         volumeMounts:
+#         - mountPath: \$mount
+#           name: data
 #       restartPolicy: Never
+#       volumes:
+#         - name: data
+#           secret:
+#             defaultMode: 0777
+#             secretName: \${name}-data
+# ---
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: \${name}-data
+# data:
+#   stdin: $i
+#   script: $s
 # EOF
-# if [[ "\$?" != "0" ]]; then
-#    exit 1
-# fi
+# [[ "\$?" != "0" ]] && exit 1
 # podname=\$(kubectl get pods --sort-by=.metadata.creationTimestamp --selector=job-name=\$name -o jsonpath="{.items[0].metadata.name}")
-# sleep 1
 # retval=1
-# for i in {1..200}; do 
+# for (( i=1; i<=\$timeout; i++ )); do 
 #   status=\$(kubectl get pod \$podname -o jsonpath='{.status.phase}') || break
 #   [[ "\$status" == "Failed" ]] && break
-#   [[ "\$status" == "Failed" ]] && retval=0 && break
+#   [[ "\$status" == "Succeeded" ]] && retval=0 && break
 #   sleep 1
 # done
-#
 # kubectl logs -f \$podname
-# 
 # exit \$retval
-#!/bin/bash
-
-echo hello from k8s
-sdf
+#
+#!/bin/sh
+echo $(cat) from $@
